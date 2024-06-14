66d8f8a441fa3a099a430f8380c9c18eac516fd8
구경열 <kyungyeol@gugyeong-yeol-ui-Macmini.local>
2024-06-15 01:04:00 +0900

    Add: sqs tag manager

---
+++ sqs-tag-manager.py
@@ -0,0 +1,48 @@
+import boto3
+from pprint import pprint
+
+
+def tag_check(queue, tag_key):
+    response = sqs.list_queue_tags(
+        QueueUrl=queue
+    )
+    tags = response['Tags']
+    return tag_key in tags
+
+
+def tag_update(queue, tag_key, tag_value):
+    print(f'update queue {queue}')
+    response = sqs.tag_queue(
+        QueueUrl=queue,
+        Tags={
+            tag_key: tag_value
+        }
+    )
+    print(response)
+
+
+"""
+sqs 리소스에 대해 특정 태그가 있는지 찾고, 없다면 태그를 부여하는 코드
+"""
+
+sqs = boto3.client('sqs')
+queue_list = sqs.list_queues()['QueueUrls']
+databricks_queue = []
+
+tag_key = 'tag_key'                       # 원하는 값으로 설정하세요.
+tag_value = 'tag_value'                   # 원하는 값으로 설정하세요.
+
+
+for queue in queue_list:
+    if 'databricks' in queue:
+        databricks_queue.append(queue)
+
+for queue in databricks_queue:
+    if tag_check(queue=queue, tag_key=tag_key):
+        pass
+    else:
+        tag_update(
+            queue=queue,
+            tag_key=tag_key,
+            tag_value=tag_value
+        )
